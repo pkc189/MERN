1d7be24c556953684f463c136e33bb5869dabf62
pkc189 <pankajchauhan189@gmail.com>
2021-01-31 15:06:36 +0530

    header footer images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
+import {Container} from 'react-bootstrap';
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = ()=> {
 
     return (
 <>
+<Header/>
 
+<Container><main className='py-3'>
+<h1>Welcome</h1></main>
+</Container>
+
+
+<Footer/>
 </>
     	);
   

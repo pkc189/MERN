8b786bc95994def32035dcd595bf14295adc3cde
pkc189 <pankajchauhan189@gmail.com>
2021-01-31 21:57:31 +0530

    Product detail screen

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,35 +1,36 @@
-import React from 'react';
-import {Navbar,Nav,Container} from 'react-bootstrap';
-import {LinkContainer} from 'react-router-bootstrap';
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 const Header = () => {
-	return(
-<><header>
+  return (
+    <>
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+          <Container>
+            <LinkContainer to="/">
+              <Navbar.Brand>ProShop</Navbar.Brand>
+            </LinkContainer>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ml-auto">
+                <LinkContainer to="/cart">
+                  <Nav.Link>
+                    <i className="fas fa-shopping-cart"></i>Cart
+                  </Nav.Link>
+                </LinkContainer>
 
-
-<Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
-<Container>
-<LinkContainer to='/'>
-  <Navbar.Brand >ProShop</Navbar.Brand>
-
-</LinkContainer>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="ml-auto">
-   <LinkContainer to='/cart'>
-      <Nav.Link><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
-</LinkContainer>
-
-<LinkContainer to='/signin'>
-      <Nav.Link><i className='fas fa-user'></i>Signin</Nav.Link>
-     </LinkContainer> 
-    </Nav>
-   
-  </Navbar.Collapse>
-  </Container>
-</Navbar>
-
-</header></>
-		)
-}
+                <LinkContainer to="/signin">
+                  <Nav.Link>
+                    <i className="fas fa-user"></i>Signin
+                  </Nav.Link>
+                </LinkContainer>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
+    </>
+  );
+};
 
 export default Header;
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import Rating from "./Rating";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 const Product = ({ product }) => {
 	return (
 		<Card className="rounded p-3 my-3">

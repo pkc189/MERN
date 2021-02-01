e31ee0160d2093996027610a418379eac9e0348f
pkc189 <pankajchauhan189@gmail.com>
2021-02-01 08:34:14 +0530

    fetching products from react(useEffect)

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import products from "../products";
-import Product from "../components/Product";
+import Product from '../components/Product.js'
 import { Row, Col } from "react-bootstrap";
+import axios from 'axios'
+
+
+
+
+
 const HomeScreen = () => {
+
+const [products,setProducts]=useState([])
+
+
+useEffect(()=>{
+
+const fetchProducts = async () =>{
+	const {data} = await axios.get('/api/products/');
+	console.log(data)
+	setProducts(data);
+}
+fetchProducts()
+
+},[])
+
+
 	return (
 		<div>
 			<h1>Latest Product</h1>
